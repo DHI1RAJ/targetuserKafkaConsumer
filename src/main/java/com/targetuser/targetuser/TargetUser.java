48325180b37585610271d2e06bddbf2a55b312a7
Dhiraj Kumar Singh <dhiraj.k.singh@Dhirajs-MacBook-Air.local>
2025-06-24 16:26:38 +0530

    Initial commit

---
+++ src/main/java/com/targetuser/targetuser/TargetUser.java
@@ -0,0 +1,13 @@
+package com.targetuser.targetuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargetUser {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TargetUser.class, args);
+	}
+
+}
